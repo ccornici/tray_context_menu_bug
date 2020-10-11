e8753b0da0f926309729a05d0c85ba3cb50b48cd
Claudiu <test@test.com>
2020-10-11 14:58:35 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tray_context_menu_bug
 Showcases a bug with the WPF + WinForms ContextMenuStrip using .NET Core 3.1
+
+Simply open with VS 2019 and run it. See "demo.mp4"
---
+++ TrayContextMenuBug/App.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using winForms = System.Windows.Forms;
+
+namespace TrayContextMenuBug
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private bool _isExit;
+        private winForms.NotifyIcon _notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+
+            _notifyIcon = new winForms.NotifyIcon();
+            _notifyIcon.DoubleClick += (s, args) => ShowMainWindow();
+            _notifyIcon.Icon = SystemIcons.Information;
+            _notifyIcon.Visible = true;
+            CreateContextMenu();
+
+            MainWindow.Show();
+        }
+
+        private void CreateContextMenu()
+        {
+            _notifyIcon.ContextMenuStrip = new winForms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("MainWindow").Click += (s, e) => ShowMainWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+        }
+
+        private void ExitApplication()
+        {
+            _isExit = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+
+        private void ShowMainWindow()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                {
+                    MainWindow.WindowState = WindowState.Normal;
+                }
+                MainWindow.Activate();
+            }
+            else
+            {
+                MainWindow.Show();
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide(); // A hidden window can be shown again, a closed one not
+            }
+        }
+    }
+}
